beb682846cbc449dc9e14c0c0ea2738ce4a5a5e1
kirthikesh <96760739+kirthikesh@users.noreply.github.com>
2022-11-28 16:01:50 +0000

    Initial commit

---
+++ Constants.py
@@ -0,0 +1,3 @@
+serverName = "DESKTOP-98CSQ3G\SQLEXPRESS"
+databaseName = "BlockChainBasedAccessControlSystemV1"
+connString = 'Driver={SQL Server};Server='+serverName+';Integrated_Security=true;Database='+databaseName
